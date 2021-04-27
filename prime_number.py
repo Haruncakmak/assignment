10b9be5cadcebcfa602c6ef3abdadde48d5cae74
HarunCAKMAK <hranckmk@gmail.com>
2021-04-28 02:01:47 +0300

    Assignment -008/4

---
+++ prime_number.py
@@ -0,0 +1,12 @@
+number = int(input("enter a number() :"))
+if number >= 2 :
+    for i in range(2,number):
+        if (number % i) == 0 : 
+            print("{} is a prime number".format(number))
+            break
+        else :
+            print("{}  is a prime number".format(number))
+            break
+else:
+    print("{} is not a prime number".format(number))
+    
